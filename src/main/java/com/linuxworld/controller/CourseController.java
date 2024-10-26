90225c65499adf22cfbec0aa0832f4dc1180f9cd
rahulraip <rahulraipurkar735@gmail.com>
2024-10-26 19:20:07 +0530

    Commit1

--- src/main/java/com/linuxworld/controller/CourseController.java
+++ src/main/java/com/linuxworld/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello LW , Workshop of Devops FINAL LW  changes !! ";
+        return "Hello! Welcome to the Courses Web App. ";
     }
 
 
