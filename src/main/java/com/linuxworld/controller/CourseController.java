8a472f97d00182209102f3ce9499030f7c057feb
rahulraip <rahulraipurkar735@gmail.com>
2024-10-27 15:59:42 +0530

    3rd commit

--- src/main/java/com/linuxworld/controller/CourseController.java
+++ src/main/java/com/linuxworld/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello! Welcome to the Courses Web Application! The Site is up.. ";
+        return "Hello! Welcome to the Courses Web Application! Its possible! ";
     }
 
 
